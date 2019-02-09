6e884f8ab0c4c2da48fac6ff1ee2cb7e85c3e343
Dan Kempster <me@dankempster.co.uk>
2019-02-09 12:21:52 +0000

    Create docker-playbook.sh and make build.sh repo specific

--- build.sh
+++ build.sh
@@ -21,52 +21,18 @@ errorExit() {
 	exit 1
 }
 
-baseImage=""
-playbook="playbook.yml"
-repository=""
-
-
-cleanUp() {
-	docker rm -v $(docker stop $1)
-}
-
-cleanUpTrap() {
-	echo ""
-	echo ""
-	echo ""
-	echo "Recieved signal... cleaning up..."
-	echo ""
-	cleanUp
-	exit 1
-}
-
-startBase() {
-	docker run -dt -v $(pwd):/project $1 bash -i
-}
-
 usage() {
-	echo "USAGE: ${PROGNAME} [-p PLAYBOOK] -b BASE_IMAGE -r REPOSITORY[:TAG]"
+	echo "USAGE: ${PROGNAME} [-t TAG]"
 }
 
-if [ $# -eq 0 ]; then
-	echo "ERROR: Missing arguments" 1>&2
-    usage
-    exit 1
-fi
+repoName="dankempster/jenkins"
+tag="build"
 
 while [ "$1" != "" ]; do
     case $1 in
-        -b | --base )
+        -t | --tag )
 			shift
-            baseImage=$1
-            ;;
-        -p | --playbook )
-			shift
-            playbook=$1
-            ;;
-        -r | --repository )
-			shift
-            repository=$1
+            tag=$1
             ;;
         * )                   
             usage
@@ -75,21 +41,8 @@ while [ "$1" != "" ]; do
     shift
 done
 
-containerId=$(startBase $baseImage)
+baseImage="geerlingguy/docker-debian9-ansible:latest"
 
-# Clean up the continer if we recieved one of these signals before
-#   terminating the script
-trap "cleanUpTrap $continerId" SIGHUP SIGINT SIGTERM
-
-echo "Building in container: ${containerId}"
-
-# Run the playbook
-docker exec -t -w /project $containerId ansible-playbook $playbook
-
-# Create the image from the container
-docker commit -p -a "Dan Kempster <me@dankempster.co.uk>" $containerId $repository
-
-echo "Used $containerId to create ${repository}"
-
-# Clean up
-cleanUp $containerId
+docker pull $baseImage
+echo ""
+bin/docker-playbook.sh -p playbook.yml -b $baseImage -r $repoName:$tag
